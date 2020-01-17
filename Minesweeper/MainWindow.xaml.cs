79d88b2f9ff74c2b2693660d7d039acf80c2ecf3
Kamil Chachurski <kamil.chachurski@microsoft.wsei.edu.pl>
2020-01-17 03:14:16 +0100

    Added board class

--- Minesweeper/MainWindow.xaml.cs
+++ Minesweeper/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ namespace Minesweeper {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+            Board board = new Board(10, 24, grid);
         }
     }
 }
