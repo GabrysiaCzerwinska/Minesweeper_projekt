7e6f6240db0935bd3fad0a0fda4b898d903dd251
MrKubaD <jakub.dedio@microsoft.wsei.edu.pl>
2020-01-17 01:09:57 +0100

    some tests of new control

--- Minesweeper/MainWindow.xaml.cs
+++ Minesweeper/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,36 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Minesweeper {
+    public class TestClass
+    {
+        public string TestString { get; set; }
+    }
     public partial class MainWindow : Window {
+        public int playAreaHeight { get; set; } = 10;
+        public int playAreaWidth { get; set; } = 10;
+        public List<TestClass> testGrid { get; set; } = new List<TestClass>();
+
         public MainWindow() {
+            generateTestGrid();
             InitializeComponent();
+            DataContext = this;
+        }
+        private void generateTestGrid()
+        {
+            for(int i = 1; i <= playAreaHeight * playAreaWidth; ++i)
+            {
+                testGrid.Add(new TestClass { TestString = i.ToString() });
+            }
+        }
+
+        private void Test1(object sender, RoutedEventArgs e)
+        {
+            var dupa = (sender as Minesweeper.Controls.FieldControl);
+            Debug.WriteLine("Matko działa!");
+        }
+        private void Test2(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Matko działa drugi przycisk!");
         }
     }
 }
